f6f317a28e0b3629f398494a6b69378e876d8af6
huangrz0702 <e0774177@u.nus.edu>
2024-04-24 14:34:49 +0800

    pipeline update

--- ETL/01_Data_Collection/02_Refresh_Model.py
+++ ETL/01_Data_Collection/02_Refresh_Model.py
@@ -1,16 +1,13 @@
 import json
-import requests
 import pandas as pd
-import numpy as np
-# import matplotlib.pyplot as plt
-import re
 from datetime import datetime
 from airflow.decorators import dag, task
 import os
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from google.cloud import bigquery
-from google.oauth2 import service_account
 import time
+import pickle
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
 
 
 # Define the default arguments for the DAG
@@ -37,7 +34,6 @@ def model_refresh_etl():
 
     @task() 
     def feature_engineering(download_path: str):
-        
         csv_file_path = os.path.join(download_path, 'processed_data/filtered_df3.csv')
         
         df1 = pd.read_csv(csv_file_path)
@@ -99,9 +95,59 @@ def model_refresh_etl():
         with open(json_file_path, 'w') as json_file:
             json.dump(scaling_dic, json_file)
             
+        newdf_file_path = os.path.join(download_path, 'processed_data/filtered_df4.csv')
+        
+        # Store df1 in csv file
+        df1.to_csv(newdf_file_path, index=False)
+
         return download_path
     
+    
+    @task() 
+    def model_retrain(download_path: str):
+        csv_file_path = os.path.join(download_path, 'processed_data/filtered_df4.csv')
+        
+        df = pd.read_csv(csv_file_path)
+        
+        columns_of_interest = ['avg_storey_range', 'floor_area_sqm', 'normalized_resale_price',
+            'total_dwelling_units', 'vacancy', 'commercial', 'mrt_interchange',
+            'flat_model', 'town', 'age_of_flat']
+            
+        filtered_features = ['floor_area_sqm', 'avg_storey_range', 'total_dwelling_units',
+            'commercial', 'age_of_flat', 'flat_model_model a',
+            'flat_model_new generation', 'flat_model_other',
+            'flat_model_premium apartment', 'town_jurong west', 'town_other',
+            'town_punggol', 'town_sengkang', 'town_tampines', 'town_woodlands',
+            'town_yishun']
+        df = df[columns_of_interest]
+        df_ohe = pd.get_dummies(df, drop_first=True)
+         
+        diff = set(filtered_features) - set(df_ohe.columns)
+
+        for column in diff:
+            df_ohe[column] = 0
+            
+        X = df_ohe[filtered_features]
+        y = df_ohe['normalized_resale_price']
+        lr = LinearRegression()
+        lr.fit(X, y)
+        knn = KNeighborsRegressor(n_neighbors=8, weights='distance')
+        knn.fit(X, y)
+        # save models as pkl
+        pkl_path_1 = os.path.join(download_path, 'processed_data/linear_model.pkl')
+        pkl_path_2 = os.path.join(download_path, 'processed_data/knn_model.pkl')
+
+        with open(pkl_path_1, 'wb') as f:
+            pickle.dump(lr, f)
+        with open(pkl_path_2, 'wb') as f:
+            pickle.dump(knn, f)
+            
+        return download_path
+    
+    
     download_path = feature_engineering("/Users/renzhou/Downloads/Y3S2/IS3107/Data-Engine/ETL/01_Data_Collection/01_dataset")
+    model_retrain(download_path)
+        
 
 data_collection_dag = model_refresh_etl()
         
